f6ab751f04c0452a6765da078558b9b9b39fc6b5
Nolan <bugygamer.mc@gmail.com>
2023-05-16 21:09:57 -0600

    Complete Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
+import { parseWithOptions } from "date-fns/fp";
 import React from "react";
+import links from "./Links";
 
-function About() {
+function About(props) {
+ 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {links(props)}
     </div>
   );
 }
 
 export default About;
+
